32bbc355c3a490fbfd8097d3fe0ee3cbd0bbe2a7
Anthony Angelo <anthonyangelo@yahoo.com>
2022-01-04 20:57:40 -0500

    Update

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # Create the Heracles Cluster using our module
-# module "heracles" {
-#   source          = "./modules/heracles"
-#   region          = "${var.region}"
-#   instance_count  = "${var.instance_count}"
-#   amisize         = "t2.large"
-#   volumesize      = "50"
-#   vpc_cidr        = "15.0.0.0/16"
-#   subnetaz        = "${var.subnetaz}"
-#   subnet_cidr     = "15.0.1.0/24"
-#   key_name        = "${var.cluster_name}-${var.region}"
-#   public_key_path = "${var.public_key_path}"
-#   cluster_name    = "${var.cluster_name}"
-#   cluster_id      = "${var.cluster_name}-${var.region}"
-# }
+module "heracles" {
+  source          = "./modules/heracles"
+  region          = "${var.region}"
+  instance_count  = "${var.instance_count}"
+  amisize         = "t2.large"
+  volumesize      = "50"
+  vpc_cidr        = "15.0.0.0/16"
+  subnetaz        = "${var.subnetaz}"
+  subnet_cidr     = "15.0.1.0/24"
+  key_name        = "${var.cluster_name}-${var.region}"
+  public_key_path = "${var.public_key_path}"
+  cluster_name    = "${var.cluster_name}"
+  cluster_id      = "${var.cluster_name}-${var.region}"
+}
 
 # Output some useful variables for quick SSH access etc.
 output "metasploit3-public_ip" {
--- modules/01-tags.tf
+++
@@ -1,7 +0,0 @@
-# Define our common tags.
-locals {
-  common_tags = "${map(
-    "HeraclesClusterName", "${var.cluster_name}",
-    "HeraclesClusterID", "${var.cluster_id}"
-  )}"
-}
