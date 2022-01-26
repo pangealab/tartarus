8a5cd59c082d1df8408031a657f73dbb5c6620a6
Anthony Angelo <anthonyangelo@yahoo.com>
2022-01-25 21:26:59 -0500

    Update

--- modules/tartarus/99-outputs.tf
+++ modules/tartarus/99-outputs.tf
@@ -8,10 +8,6 @@ output "metasploit3windows-public_ip" {
   value = aws_eip.metasploit3windows_eip.public_ip
 }
 
-output "metasploit3windows-public_ip" {
-  value = aws_eip.metasploit3windows_eip.public_ip
-}
-
 output "control-public_ip" {
   value = aws_eip.control_eip.public_ip
 }
