688a62af9a87713c70f8bf4319eb0869dc2f7e45
Anthony Angelo <anthonyangelo@yahoo.com>
2022-01-25 22:55:58 -0500

    Update

--- modules/tartarus/99-outputs.tf
+++ modules/tartarus/99-outputs.tf
@@ -1,4 +1,7 @@
 # Output some useful variables for quick SSH access etc.
+output "control-public_ip" {
+  value = aws_eip.control_eip.public_ip
+}
 
 output "metasploit3ubuntu-public_ip" {
   value = aws_eip.metasploit3ubuntu_eip.public_ip
@@ -6,8 +9,4 @@ output "metasploit3ubuntu-public_ip" {
 
 output "metasploit3windows-public_ip" {
   value = aws_eip.metasploit3windows_eip.public_ip
-}
-
-output "control-public_ip" {
-  value = aws_eip.control_eip.public_ip
 }
