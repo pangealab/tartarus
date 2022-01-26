8640e4e7e9ba861d7d063f039e21b38a97d7c073
Anthony Angelo <anthonyangelo@yahoo.com>
2022-01-25 22:56:02 -0500

    Update

---
+++ modules/tartarus/08-inventory.tf
@@ -0,0 +1,12 @@
+//  Collect together all of the output variables needed to build to the final
+//  inventory from the inventory template
+
+resource "local_file" "inventory" {
+ content = templatefile("inventory.template.cfg", {
+      control-public_ip = aws_eip.control_eip.public_ip,
+      metasploit3ubuntu-public_ip = aws_eip.metasploit3windows_eip.public_ip,
+      metasploit3windows-public_ip = aws_eip.metasploit3windows_eip.public_ip
+  }
+ )
+ filename = "inventory-${var.cluster_name}.cfg"
+}
